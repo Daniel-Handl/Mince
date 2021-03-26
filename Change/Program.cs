e78c066af976461088dceda8450dfdd74807f6a9
Daniel Handl <handl.daniel.2017@skola.ssps.cz>
2021-03-26 01:20:27 +0100

    Přidání rekurentní metody

--- Change/Program.cs
+++ Change/Program.cs
@@ -14,15 +14,30 @@ namespace Change
             int cena = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Zadej kolik zákatník zaplatil:");
             int platba = Convert.ToInt32(Console.ReadLine());
-
-            Change(cena, platba);
+            int[] pole = new int[] { 50, 20, 10, 5, 2, 1 };
+            Change(cena, platba, pole);
+            Change_recurrent (platba - cena, 0, pole);
             Console.ReadLine();
         }
 
-        private static void Change(int cena, int platba)
+        private static void Change_recurrent(int vratit,int index, int[] pole)
+        {
+            int zbytek = vratit % pole[index];
+            int vypis = (vratit - zbytek) / pole[index];
+            if (vypis >= 1)
+            {
+                Console.WriteLine(vypis + " x " + pole[index] + "Kč");
+            }
+            if (zbytek > 0)
+            {
+                Change_recurrent(zbytek, ++index, pole);
+            }
+
+        }
+
+        private static void Change(int cena, int platba, int[] pole)
         {
             int vratit = platba - cena;
-            int[] pole = new int[] { 50, 20, 10, 5, 2, 1 };
             int index = 0;
             int zbytek = vratit;
             while (zbytek > 0)
