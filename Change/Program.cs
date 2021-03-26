817e90ad0bfda2cf4ace8a0e801608acfc8bc541
Daniel Handl <handl.daniel.2017@skola.ssps.cz>
2021-03-26 01:02:04 +0100

    realizace diagramu

--- Change/Program.cs
+++ Change/Program.cs
@@ -10,7 +10,32 @@ namespace Change
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Zadej cenu nákupu:");
+            int cena = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Zadej kolik zákatník zaplatil:");
+            int platba = Convert.ToInt32(Console.ReadLine());
 
+            Change(cena, platba);
+            Console.ReadLine();
+        }
+
+        private static void Change(int cena, int platba)
+        {
+            int vratit = platba - cena;
+            int[] pole = new int[] { 50, 20, 10, 5, 2, 1 };
+            int index = 0;
+            int zbytek = vratit;
+            while (zbytek > 0)
+            {
+                zbytek = vratit % pole[index];
+                int vypis = (vratit - zbytek) / pole[index];
+                if (vypis>=1)
+                {
+                    Console.WriteLine(vypis + " x " + pole[index] + "Kč");
+                }
+                vratit = zbytek;
+                index++;
+            }
         }
     }
 }
